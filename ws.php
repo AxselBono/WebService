f164132472c04ee92f94482ce091c85ab277d5e2
Axsel Bono Cuevas Ferreyra <91909664+AxselBono@users.noreply.github.com>
2022-03-03 17:44:04 +0100

    Add files via upload

---
+++ ws.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once("modules/getparams.php");
+include_once("modules/tools2.php");
+
+include_once("modules/validate2.php");
+include_once("modules/execute3.php");
+include_once("modules/error.php");
+include_once("modules/a.php");
+
+
+
+?>
